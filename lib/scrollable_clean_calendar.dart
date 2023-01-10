25c4cdd09aa49d41768a14789f96228e0991a1d3
1mavic <amacegora@gmail.com>
2023-01-11 00:07:01 +0600

    Update scrollable_clean_calendar.dart

--- lib/scrollable_clean_calendar.dart
+++ lib/scrollable_clean_calendar.dart
@@ -138,6 +138,7 @@ class _ScrollableCleanCalendarState extends State<ScrollableCleanCalendar> {
 
   Widget listViewCalendar() {
     return ListView.separated(
+      physics: const NeverScrollableScrollPhysics(),
       controller: widget.scrollController,
       padding: widget.padding ??
           const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
